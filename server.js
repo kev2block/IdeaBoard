5bf1b7d064b0e6a1ece2bd5df4f4e90aa77afa6c
Lucas <68608785+kev2block@users.noreply.github.com>
2024-07-14 23:34:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
-const https = require('https');
 const http = require('http');
 const { Server } = require('socket.io');
 
