9fe54033353861342b3fc222f9705c869b5f836c
Lucas <68608785+kev2block@users.noreply.github.com>
2024-07-14 19:45:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const axios = require('axios');
 const cookieParser = require('cookie-parser');
@@ -15,14 +17,14 @@ const redirectUri = process.env.REDIRECT_URI;
 const allowedUsers = process.env.ALLOWED_USERS.split(',');
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
 
-app.get('/login', (req, res) => {
+app.get('/api/login', (req, res) => {
   const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
   res.redirect(twitchAuthUrl);
 });
 
-app.get('/callback', async (req, res) => {
+app.get('/api/callback', async (req, res) => {
   const code = req.query.code;
 
   try {
@@ -49,7 +51,7 @@ app.get('/callback', async (req, res) => {
 
     if (allowedUsers.includes(user.login)) {
       res.cookie('twitch_user', user.login, { maxAge: 900000, httpOnly: true });
-      res.redirect('https://your-project-name.vercel.app');
+      res.redirect('/');
     } else {
       res.send('Access denied');
     }
@@ -58,7 +60,7 @@ app.get('/callback', async (req, res) => {
   }
 });
 
-app.get('/check-login', (req, res) => {
+app.get('/api/check-login', (req, res) => {
   const user = req.cookies.twitch_user;
   if (user && allowedUsers.includes(user)) {
     res.send(user);
@@ -68,7 +70,7 @@ app.get('/check-login', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 app.listen(port, () => {
