24a44aee0fdca335eb84ec06b981d5c390cfbb23
Lucas <68608785+kev2block@users.noreply.github.com>
2024-07-14 23:31:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
 const port = process.env.PORT || 8080;
 
-// Ersetzen Sie 'YOUR_MONGODB_CONNECTION_STRING' durch Ihren tatsächlichen Verbindungsstring
 const mongoUri = 'mongodb+srv://kev2block:8g03QtRl4grvaHy9@ideaboard.vdip7wi.mongodb.net/?retryWrites=true&w=majority&appName=IdeaBoard';
 
 mongoose.connect(mongoUri, {
@@ -24,7 +22,6 @@ mongoose.connect(mongoUri, {
   console.error('Error connecting to MongoDB', err);
 });
 
-// Define Schemas
 const ideaSchema = new mongoose.Schema({
   title: String,
   description: String,
@@ -43,16 +40,13 @@ const fieldSchema = new mongoose.Schema({
   color: String
 });
 
-// Define Models if not already defined
-const Idea = mongoose.models.Idea || mongoose.model('Idea', ideaSchema);
-const Field = mongoose.models.Field || mongoose.model('Field', fieldSchema);
+const Idea = mongoose.model('Idea', ideaSchema);
+const Field = mongoose.model('Field', fieldSchema);
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes for ideas
 app.get('/ideas', async (req, res) => {
   const ideas = await Idea.find();
   res.json(ideas);
@@ -65,17 +59,16 @@ app.post('/ideas', async (req, res) => {
     updatedAt: new Date()
   });
   await newIdea.save();
-  io.emit('newIdea', newIdea);  // Emit new idea event to all connected clients
+  io.emit('newIdea', newIdea);
   res.status(201).json(newIdea);
 });
 
 app.delete('/ideas/:id', async (req, res) => {
   await Idea.findByIdAndDelete(req.params.id);
-  io.emit('deleteIdea', req.params.id);  // Emit delete idea event to all connected clients
+  io.emit('deleteIdea', req.params.id);
   res.status(204).send();
 });
 
-// Routes for fields
 app.get('/fields', async (req, res) => {
   const fields = await Field.find();
   res.json(fields);
@@ -84,30 +77,30 @@ app.get('/fields', async (req, res) => {
 app.post('/fields', async (req, res) => {
   const newField = new Field(req.body);
   await newField.save();
-  io.emit('newField', newField);  // Emit new field event to all connected clients
+  io.emit('newField', newField);
   res.status(201).json(newField);
 });
 
 app.delete('/fields/:id', async (req, res) => {
   await Field.findByIdAndDelete(req.params.id);
-  io.emit('deleteField', req.params.id);  // Emit delete field event to all connected clients
+  io.emit('deleteField', req.params.id);
   res.status(204).send();
 });
 
-// Serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-// Handle socket.io connections
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
